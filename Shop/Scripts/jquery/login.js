f3c4582087dee7d4647f9529147cb3e8e94c3250
admin <admin@B-WELL-010>
2022-06-09 17:51:57 +0900

    jQueryを開始しました。

---
+++ Shop/Scripts/jquery/login.js
@@ -0,0 +1,6 @@
+﻿$(function () {
+    $("form").prepend("<h1>Magazon</h1>");
+    $("h1").css("color", "orange");
+    $("h1").css("font-size", "48px");
+    $(".btn").css("font-weight","bold");
+});
